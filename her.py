00945a4107ac4a8529a9196897b27113302b9f95
Isaac Chacko <isaac.chacko05@gmail.com>
2024-11-10 09:32:33 -0600

    add her main file

---
+++ her.py
@@ -0,0 +1,27 @@
+
+class RandomAgent:
+    def __init__(self, player=PLAYER1):
+        self.player = player
+
+    # given the game state, gets all of the possible moves
+    def get_possible_moves(self, game):
+        """Returns list of all possible moves in current state."""
+        moves = []
+        current_pieces = game.p1_pieces if game.current_player == PLAYER1 else game.p2_pieces
+
+        if current_pieces < NUM_PIECES:
+            # placement moves
+            for r in range(BOARD_SIZE):
+                for c in range(BOARD_SIZE):
+                    if game.board[r][c] == EMPTY:
+                        moves.append((r, c))
+        else:
+            # movement moves
+            for r0 in range(BOARD_SIZE):
+                for c0 in range(BOARD_SIZE):
+                    if game.board[r0][c0] == game.current_player:
+                        for r1 in range(BOARD_SIZE):
+                            for c1 in range(BOARD_SIZE):
+                                if game.board[r1][c1] == EMPTY:
+                                    moves.append((r0, c0, r1, c1))
+        return moves
