b17ce612376f590940e73989c43d37854865046d
Isaac Chacko <isaac.chacko05@gmail.com>
2024-11-10 12:46:05 -0600

    as

--- cases.py
+++ cases.py
@@ -4,6 +4,40 @@ import numpy as np
 import time
 import logging
 
+
+class RandomAgent:
+    def __init__(self, player=PLAYER1):
+        self.player = player
+
+    # given the game state, gets all of the possible moves
+    def get_possible_moves(self, game):
+        """Returns list of all possible moves in current state."""
+        moves = []
+        current_pieces = game.p1_pieces if game.current_player == PLAYER1 else game.p2_pieces
+
+        if current_pieces < NUM_PIECES:
+            # placement moves
+            for r in range(BOARD_SIZE):
+                for c in range(BOARD_SIZE):
+                    if game.board[r][c] == EMPTY:
+                        moves.append((r, c))
+        else:
+            # movement moves
+            for r0 in range(BOARD_SIZE):
+                for c0 in range(BOARD_SIZE):
+                    if game.board[r0][c0] == game.current_player:
+                        for r1 in range(BOARD_SIZE):
+                            for c1 in range(BOARD_SIZE):
+                                if game.board[r1][c1] == EMPTY:
+                                    moves.append((r0, c0, r1, c1))
+        return moves
+
+    def get_best_move(self, game):
+        """Returns a random valid move."""
+        possible_moves = self.get_possible_moves(game)
+        return random.choice(possible_moves)
+
+
 # Set up logging
 logging.basicConfig(filename='test_cases_1.log',
                     level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -560,6 +594,9 @@ def run_cases(game,
               turn_count,
               attempt_number,
               lastFitness):
+
+    if turn_count == 1 or turn_count == 2:
+        return False
 
     heatmap_array = combine_heatmaps(
         [Ishaan_heatmap, RadialConvolution], game, board, turn_count, attempt_number, lastFitness)
--- her.py
+++ her.py
@@ -1,12 +1,23 @@
 from PushBattle import Game, PLAYER1, PLAYER2, EMPTY, BOARD_SIZE, NUM_PIECES, _torus
 import cases
 from cases import get_best_move_and_fitness, run_cases
+from random_agent import RandomAgent
+
 
 class HisAgent:
     def __init__(self, player=PLAYER1):
         self.player = player
         self.lastFitness = 0
-
+        self.random = RandomAgent()
     # given the game state, gets all of the possible moves
+
     def get_best_move(self, game, board, turn_count, attempt_number):
-        move, self.lastFitness = get_best_move_and_fitness(run_cases(game, board, turn_count, attempt_number, self.lastFitness))
+        result = get_best_move_and_fitness(
+            run_cases(game, board, turn_count, attempt_number, self.lastFitness))
+
+        if result == False:
+            return self.random.get_best_move(game)
+            self.lastFitness = 1
+
+        else:
+            move, self.lastFitness = result
