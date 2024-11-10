58c21ee4aa763e8d96efdb7facba4debcaacb73b
Ju5t1nL3 <justinle2006@gmail.com>
2024-11-10 12:33:06 -0600

    add

--- her.py
+++ her.py
@@ -1,6 +1,6 @@
 from PushBattle import Game, PLAYER1, PLAYER2, EMPTY, BOARD_SIZE, NUM_PIECES, _torus
 import cases
-from cases import get_best_move_and_fitness
+from cases import get_best_move_and_fitness, run_cases
 
 class HisAgent:
     def __init__(self, player=PLAYER1):
@@ -9,4 +9,4 @@ class HisAgent:
 
     # given the game state, gets all of the possible moves
     def get_best_move(self, game, board, turn_count, attempt_number):
-        move, self.lastFitness = get_best_move_and_fitness(game, board, turn_count, attempt_number, self.lastFitness)
+        move, self.lastFitness = get_best_move_and_fitness(run_cases(game, board, turn_count, attempt_number, self.lastFitness))
