11ba08aecc10acf42bd81283baccfb148c467f54
William Lam <williamlam5656@gmail.com>
2024-11-09 19:02:40 -0600

    cool ass code

--- strategic_agent.py
+++ strategic_agent.py
@@ -10,7 +10,7 @@ class NEATAgent:
         self.population = neat.Population(self.config)
         self.best_genome = None
 
-    def train(self, generations=50):
+    def train(self, generations=10000):
         self.population.add_reporter(neat.StdOutReporter(True))
         stats = neat.StatisticsReporter()
         self.population.add_reporter(stats)
@@ -32,19 +32,30 @@ class NEATAgent:
         return game.p1_pieces - game.p2_pieces  # Simple fitness function
 
     def get_board_state(self, game):
-        return [item for sublist in game.board for item in sublist]
+        current_player = PLAYER1 if game.current_player == 1 else PLAYER2
+        opponent = PLAYER2 if current_player == PLAYER1 else PLAYER1
+        return [1 if item == current_player else (-1 if item == opponent else 0) for sublist in game.board for item in sublist]
 
     def interpret_output(self, output, game):
-        if game.p1_pieces < NUM_PIECES:
+        current_player = PLAYER1 if game.current_player == 1 else PLAYER2
+        if (current_player == PLAYER1 and game.p1_pieces < NUM_PIECES) or (current_player == PLAYER2 and game.p2_pieces < NUM_PIECES):
             # Placement phase
             move_index = output.index(max(output))
             return (move_index // BOARD_SIZE, move_index % BOARD_SIZE)
         else:
             # Movement phase
             from_index = output.index(max(output[:BOARD_SIZE**2]))
-            to_index = output.index(max(output[BOARD_SIZE**2:]))
-            return (from_index // BOARD_SIZE, from_index % BOARD_SIZE,
-                    to_index // BOARD_SIZE, to_index % BOARD_SIZE)
+            to_index = BOARD_SIZE**2 + output[BOARD_SIZE**2:].index(max(output[BOARD_SIZE**2:]))
+            
+            from_row, from_col = from_index // BOARD_SIZE, from_index % BOARD_SIZE
+            to_row, to_col = (to_index - BOARD_SIZE**2) // BOARD_SIZE, (to_index - BOARD_SIZE**2) % BOARD_SIZE
+            
+            # Ensure the 'from' position contains the player's piece
+            if game.board[from_row][from_col] != current_player:
+                # If not, choose a random valid move
+                return self.get_random_move(game)
+            
+            return (from_row, from_col, to_row, to_col)
 
     def get_best_move(self, game):
         if self.best_genome is None:
@@ -61,7 +72,8 @@ class NEATAgent:
 
     def get_possible_moves(self, game):
         moves = []
-        if game.p1_pieces < NUM_PIECES:
+        current_player = PLAYER1 if game.current_player == 1 else PLAYER2
+        if game.p1_pieces < NUM_PIECES or game.p2_pieces < NUM_PIECES:
             for r in range(BOARD_SIZE):
                 for c in range(BOARD_SIZE):
                     if game.board[r][c] == EMPTY:
@@ -69,7 +81,7 @@ class NEATAgent:
         else:
             for r0 in range(BOARD_SIZE):
                 for c0 in range(BOARD_SIZE):
-                    if game.board[r0][c0] == PLAYER1:
+                    if game.board[r0][c0] == current_player:
                         for r1 in range(BOARD_SIZE):
                             for c1 in range(BOARD_SIZE):
                                 if game.board[r1][c1] == EMPTY:
